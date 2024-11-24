283fa9d0cb3618b8b3569263d93d2f6479b55e02
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-23 21:43:31 -0800

    initial scene design

--- docs/foodconomy/main.js
+++ docs/foodconomy/main.js
@@ -11,11 +11,42 @@ characters = [];
 options = {
   viewSize: { x: 150, y: 75 },
   isShowingScore: true,
-  theme: "pixel"
+  theme: "simple",
 };
 
 function update() {
+  // pre-game menu scene
   if (!ticks) {
-    
   }
+
+  drawScene();
+}
+
+function drawScene() {
+  // create game window size references
+  const { x: gameWidth, y: gameHeight } = options.viewSize;
+  const proportion = options.viewSize.x / 7;
+
+  // draw sky
+  color("blue");
+  rect(0, 0, gameWidth, gameHeight);
+
+  // draw ground
+  color("green");
+  rect(0, gameHeight - 10, gameWidth, 10);
+
+  // draw grocery store
+  text("store", 63, 38);
+  color("light_blue");
+  rect(3 * proportion, gameHeight - 30, proportion, 20);
+  color("black")
+  rect(3.75 * proportion, gameHeight - 18, 5, 8)
+
+  // draw houses
+  color("red")
+  rect(gameWidth * 0.8, gameHeight - 20, 10, 10)
+  rect(gameWidth * 0.9, gameHeight - 20, 10, 10)
+  color("black")
+  rect(gameWidth * 0.825, gameHeight - 15, 4, 5)
+  rect(gameWidth * 0.925, gameHeight - 15, 4, 5)
 }
