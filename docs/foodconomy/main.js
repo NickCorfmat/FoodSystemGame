0a6704a9494008083e338af9217bdda12e79458e
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-23 21:55:19 -0800

    updated scene

--- docs/foodconomy/main.js
+++ docs/foodconomy/main.js
@@ -35,15 +35,30 @@ function drawScene() {
   color("green");
   rect(0, gameHeight - 10, gameWidth, 10);
 
+  // draw farm
+  color("black")
+  text("farm", proportion - 9, 20)
+
+  color("yellow")
+  rect(gameWidth * 0.1, gameHeight - 16, 2, 6)
+  rect(gameWidth * 0.12, gameHeight - 17, 2, 7)
+  rect(gameWidth * 0.146, gameHeight - 16, 2, 6)
+  rect(gameWidth * 0.17, gameHeight - 17, 2, 7)
+  rect(gameWidth * 0.19, gameHeight - 16, 2, 6)
+
   // draw grocery store
   color("black")
   text("store", 63, 38);
+
   color("light_blue");
   rect(3 * proportion, gameHeight - 30, proportion, 20);
   color("black")
   rect(3.75 * proportion, gameHeight - 18, 5, 8)
 
   // draw houses
+  color("black")
+  text("buyers", 5 * proportion + 8, gameHeight - 26)
+
   color("red")
   rect(gameWidth * 0.8, gameHeight - 20, 10, 10)
   rect(gameWidth * 0.9, gameHeight - 20, 10, 10)
