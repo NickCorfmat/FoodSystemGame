94b6614c4c620d30acbcef496928fd5c39745082
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-23 21:08:48 -0800

    updated title screen

--- docs/foodconomy/main.js
+++ docs/foodconomy/main.js
@@ -1,13 +1,21 @@
-title = "";
+title = "FOODCONOMY";
 
 description = `
+     Meet
+ever-increasing
+  food demand
 `;
 
 characters = [];
 
-options = {};
+options = {
+  viewSize: { x: 150, y: 75 },
+  isShowingScore: true,
+  theme: "pixel"
+};
 
 function update() {
   if (!ticks) {
+    
   }
 }
